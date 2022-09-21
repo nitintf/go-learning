a2647b5762e582ccc5c69a597394f0d81512373c
nitinpanwar08 <nitinpanwar0802@gmail.com>
2022-09-22 03:12:37 +0530

    go: primitives

--- variables/primitives.go
+++ variables/primitives.go
@@ -1,5 +1,26 @@
 package variables
 
+import "fmt"
+
 func Primitives() {
+
+	print("\n\n\n\n")
+	print("Primitives Package\n\n")
+
+	var isCompleted bool = false
+
+	fmt.Printf("%v, %T\n", isCompleted, isCompleted)
+
+	isEqual := 1 == 3
+	fmt.Printf("%v, %T\n", isEqual, isEqual)
+
+	// complex type
+	var a complex128 = 1 + 2i // complex(1, 2)
+	fmt.Printf("%v, %T\n", real(a), real(a))
+	fmt.Printf("%v, %T\n", imag(a), imag(a))
+
+	// rune - using single quotes
+	b := 'a' // var b rune = 'a
+	fmt.Printf("%v, %T\n", b, b)
 
 }
